73160864f5409b7c4c93ac90eba05ff58f554632
kaseyagopher <alghufarsanjab@gmail.com>
2025-11-13 21:06:01 +0100

    update adding crud function ending

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,16 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Product extends Model
 {
-    // Désactive l’auto-incrément pour la clé primaire
-    public $incrementing = false;
-
-    // Indique que la clé primaire est une string
-    protected $keyType = 'string';
-
+    
     // Attributs remplissables
     protected $fillable = [
         'name',
@@ -23,15 +17,5 @@ class Product extends Model
         'dish_type',
     ];
 
-    // Générer automatiquement un UUID à la création
-    protected static function boot(): void
-    {
-        parent::boot();
-
-        static::creating(function ($model) {
-            if (empty($model->{$model->getKeyName()})) {
-                $model->{$model->getKeyName()} = (string) Str::uuid();
-            }
-        });
-    }
+    
 }
